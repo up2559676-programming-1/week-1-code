72c54772338b9e143e88e938070fbdec0248f9c9
manighahrmani <mashapicasso@gmail.com>
2024-09-18 14:36:10 +0100

    Change to snake_case

--- pract1.py
+++ pract1.py
@@ -1,15 +1,15 @@
-def sayHello():
+def say_hello():
     print("Hello World")
 
 
-def sayBye():
+def say_bye():
     print("Goodbye Mars")
 
 
 # A simple kilograms to ounces conversion program
 # It asks for a weight in kilograms (for example 10)
 # and converts it to ounces (352.74)
-def kilos2Ounces():
+def kilos_to_ounces():
     kilos = float(input("Enter a weight in kilograms: "))
     ounces = kilos * 35.274
     print("The weight in ounces is", ounces)
@@ -17,13 +17,13 @@ def kilos2Ounces():
 
 def count():
     for number in range(10):
-        print("Number is now: ", number)
+        print("Number is now:", number)
 
 
 # A simple euros to pounds conversion program
 # It asks for a value in euros (for example 10)
 # and converts it to pounds (8.7)
-def euros2Pounds():
+def euros_to_pounds():
     euros = float(input("Enter a value in euros: "))
     pounds = euros * 0.87
     print("The value in pounds is", pounds)
